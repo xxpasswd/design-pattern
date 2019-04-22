270427be7517773c7dcb85b04be3bf61f71cbd77
xxpasswd <1490621113@qq.com>
2019-04-22 11:47:46 +0800

    add adapter pattern

---
+++ adapter.py
@@ -0,0 +1,61 @@
+"""
+适配器模式
+适配器模式为类提供了一个统一的接口
+
+参考文档：https://sourcemaking.com/design_patterns/adapter
+"""
+
+class Dog:
+    def __init__(self):
+        self.name = 'dog'
+
+    def bark(self):
+        print('woof')
+
+
+class Cat:
+    def __init__(self):
+        self.name = 'cat'
+
+    def meow(self):
+        print('meow')
+
+
+class Human:
+    def __init__(self):
+        self.name = 'human'
+
+    def hello(self):
+        print('hello')
+
+
+class Adapter:
+    """
+    usage:
+
+        dog = Dog()
+        adapter = Adapter(dog, make_noise=dog.bark)
+    """
+
+    def __init__(self, obj, **adpated_method):
+        self.obj = obj
+        self.__dict__.update(adpated_method)
+    
+    def __getattr__(self, attr):
+        """所有没被适配的方法都会进入这里面"""
+        return getattr(self.obj, attr)
+
+if __name__ == '__main__':
+    def main():
+        objs = []
+        dog = Dog()
+        objs.append(Adapter(dog, make_noise=dog.bark))
+        cat = Cat()
+        objs.append(Adapter(cat, make_noise=cat.meow))
+        human = Human()
+        objs.append(Adapter(human, make_noise=human.hello))
+
+        for o in objs:
+            o.make_noise()
+
+    main()
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-#### 1. 单例模式[singleton](./singleton.py)
+#### 1. 单例模式 [singleton](./singleton.py)
+#### 2. 适配器模式 [adapter](./adapter.py)
